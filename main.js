d2c27622584abf3bada208a8090cc591184ddc29
ranasingh99 <sanketverma852@gmail.com>
2023-06-05 10:02:05 +0530

    when function is inside the object

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
-// Closures
-
-var item = 10;
- function x(){
-    var val = 8;
-    function y(){
-        console.log(val);
+// call apply and bind
+let name = {
+    firstname:'Rana',
+    lastname:'singh',
+    printFullName:function(){
+        console.log(this.firstname+" "+this.lastname);
     }
-    y();
- }
+  
+}
+name.printFullName();
+
+let name2 = {
+    firstname:"Mahendra Singh",
+    lastname:"Dhoni"
+}
+
+//function borrowing
+name.printFullName.call(name2);
+//here this will point to name2 object which we have passed as parameter
