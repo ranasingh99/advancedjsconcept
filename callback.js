755a20a422a0234e9b6c864002b56046ba7857b2
ranasingh99 <sanketverma852@gmail.com>
2023-09-06 09:54:36 +0530

    solve promis hell with await

--- callback.js
+++ callback.js
@@ -1,47 +1,24 @@
-// callback hell
-// function buyacar(callback1,callback2){
-//     setTimeout(()=>{
-//         console.log('buy a car')
-//         callback1(callback2);
-//     },1000);
-// }
-// function planatrip(callback){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             console.log('plan a trip to manali')
-//             callback();
-//         },1000);
-//     })
-  
-// }
-// function reachedmanali(){
-//     setTimeout(()=>{
-//         console.log('reached manali');
-//     },1000)
-// }
-//buyacar(planatrip , reachedmanali);
+// asyn await
+// the promise hell further can be rsolve by the use of async await
 
-
-// now to solve the problem of callback hell promises came into the picture
-// function buyacar(){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             resolve('buy a car')
-//         },1000);
-//     })
-// }
-// function plantrip(){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             resolve('lets go to the manali');
-//         },2000)
-//     })
-// }
-// buyacar().then((msg1)=>{
-//     console.log(msg1);
-//     plantrip().then((msg2)=>{
-//         console.log(msg2);
-//     })
-// });
-// now this also will not make our code clean , it will create a promis hell
-// so to resolve that a new practice of AWAIT was taken into account
+function buyacar(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(' bought a car maecedes-benz')
+        },2000)
+    })
+}
+function planatrip(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve('lets go to manali')
+        },1000)
+    })
+}
+async function fun1(){
+    const msg1 = await buyacar();
+console.log(msg1);
+const msg2 = await planatrip();
+console.log(msg2);
+}
+fun1();
